0afe791441b6240cfe256cf4eadb5e73a518ac16
luz_marinazh <110910640+monskooklmzh@users.noreply.github.com>
2023-09-21 16:52:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+function tocaSomvaca (idElementAudio) {
+	document.queryselector("idElementAudio").play();
+	}
+/*
+
+*/
+	const listaDeteclas *  document.queryselectorALL(".tecla");
+
+    let contador;    	
+
+	while(contador < listaDeteclas.length){
+	{contador} onclink = tocaSomvaca;	
+    contador = contador +1;
+	console.log(contador);
+
+	}
+
+    function tocaSomgato (){
+    	document.queryselector("#som_teclas_gato").play;
+
+    }
+    
+    document.queryselector('.tecla_gato').onclink= tocaSomgato;
+
+  /*
+
+*/
+
+    function tocaSomelefante (){
+    	document.queryselector('#som_tecla_elefante').play;
+
+    }
+     document.queryselector('.tecla_elefante').onclink= tocaSomelefante;
+
+
+/*
+
+*/
+     function tocaSommacaco (){
+     	document.queryselector('#som_tecla_macaco').play;
+
+     }
+
+     document.queryselector('.tecla_macaco'). onclink= tocaSommacaco;
+
+/*
+
+*/
+	     function tocaSompato (){
+	     	document.queryselector('#som_tecla_pato').play;
+
+	     }
+
+          document.queryselector('.tecla_pato') . onclink=tocaSompato;
+/*
+
+*/
+          function tocaSomporco (){
+          	document.queryselector('#som_tecla_porco').play;
+
+          }
+
+        document.queryselector('.tecla_porco'). onclink= tocaSomporco;
+/*
+
+*/
+        function tocaSomgalinha(){
+        	document.queryselector('#som_tecla_galinha').play;
+
+        }
+        
+          document.queryselector('.tecla_galinha'). onclink= tocaSomgalinha;
+
+/*
+
+*/
+          function tocaSomcabalo(){
+          	document.queryselector('#som_tecla_cabalo').play;
+
+          }
+     
+          document.queryselector('.tecla_cabalo'). onclink= tocaSomcabalo;
+/*
+
+*/
+          function tocaSomlobo(){
+          	document.queryselector('#som_tecla_lobo').play;
+
+          }
+         document.queryselector('.tecla_lobo'). onclink= tocaSomlobo;
